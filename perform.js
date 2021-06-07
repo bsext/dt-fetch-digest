6a3ac67db1e0b85cdeaca41be1cadadbe1dddc4a
gookcompsci <gookcompsci@hotmail.com>
2021-06-07 22:56:16 +0700

    fetch

--- perform.js
+++ perform.js
@@ -1,7 +1,10 @@
 var ctx = require('../../../context');
 var Utils = ctx.getLib('lib/util/plugin-utils');
 
-var request = require("request").defaults({ encoding: null });
+const DigestFetch = require('digest-fetch')
+const NdFetch = require('node-fetch')
+
+//var request = require("request").defaults({ encoding: null });
 
 function perform_function(context,request,response){
   var job_id = context.jobconfig.job_id;
@@ -18,6 +21,7 @@ function perform_function(context,request,response){
   var req_headers = param.headers || {};
   var req_body_type = param.body_type || "json";
   var resp_encode = param.encoding || "text";
+  var req_authen = param.authen;
 
   var env = {
     'type' : output_type,
@@ -36,6 +40,7 @@ function perform_function(context,request,response){
     req_headers = (_prm.headers)?_prm.headers:req_headers;
     req_body_type = (_prm.body_type)?_prm.body_type:req_body_type;
     resp_encode = (_prm.encoding)?_prm.encoding:resp_encode;
+    req_authen = (_prm.authen)?_prm.authen:req_authen;
   }
 
   send_request({'url':req_url,
@@ -43,6 +48,7 @@ function perform_function(context,request,response){
                 'headers':req_headers,
                 'body_type':req_body_type,
                 'body':data,
+                'authen':req_authen,
                 'resp_encode':resp_encode},function(err,resp,body){
 
     var respmeta = meta;
@@ -50,7 +56,7 @@ function perform_function(context,request,response){
       if(k.startsWith('_')){delete respmeta[k];}
     });
 
-    respmeta['_status_code'] = (err)?0:resp.statusCode;
+    respmeta['_status_code'] = resp.statusCode;
     respmeta['_error'] = (err)?true:false;
     response.meta = respmeta;
 
@@ -66,7 +72,7 @@ function perform_function(context,request,response){
         response.success(body,output_type);
       }
     }else{
-      response.success(null,output_type);
+      response.success(body,output_type);
     }
 
   });
@@ -84,13 +90,16 @@ function send_request(prm,cb)
      { 'cache-control': 'no-cache' }
   };
 
+  options.authen = prm.authen;
+
   if(prm.method.toLowerCase()=='post' || prm.method.toLowerCase()=='put')
   {
     options.method = prm.method.toUpperCase();
 
     if(prm.body_type=='json' && typeof prm.body == 'object'){
       options.headers['content-type'] = 'application/json';
-      options.json = prm.body;
+      //options.json = prm.body;
+      options.body= JSON.stringify(prm.body) ;
     }else if(prm.body_type=='text' || typeof prm.body == 'string'){
       options.headers['content-type'] = 'text/plain';
       options.body = prm.body;
@@ -106,14 +115,56 @@ function send_request(prm,cb)
 
   options.encoding = (prm.resp_encode == 'binary')?null:'utf8';
 
-  request(options, function (err, resp, body) {
-    if (!err) {
-      cb(err, resp, body);
+  fetchrq(options).then((res) =>{
+    if (!res.err) {
+      cb(res.err, res.resp, res.body);
     }else{
       cb(new Error("request error"));
     }
-  });
+  })
+
+  // request(options, function (err, resp, body) {
+  //   if (!err) {
+  //     cb(err, resp, body);
+  //   }else{
+  //     cb(new Error("request error"));
+  //   }
+  // });
 
 }
+
+async function fetchrq(prm){
+
+  let ret = {'err':null,resp : {'statusCode':0},'body':null}
+
+  let url = prm.url
+  let opt = {}
+  
+  opt.headers = prm.headers
+  opt.method = prm.method
+  opt.body = null
+
+  let client = NdFetch
+
+  //Authen
+  if(typeof prm.authen == 'object' && prm.authen.type == 'digest'){
+    client = new DigestFetch(prm.authen.username,prm.authen.password)
+  }
+
+  let response = await client.fetch(url,opt)
+  let respdata = await response.buffer()
+
+  if(response.status >= 200 && response.status < 300){ret.err = null}else{ret.err=true}
+  ret.resp.statusCode = response.status
+  ret.body = respdata
+
+  if(respdata && prm.encoding == 'utf8'){
+    ret.body = respdata.toString('utf8')
+  }
+
+
+  return ret
+}
+
 
 module.exports = perform_function;
